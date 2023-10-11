64e3050932699c6cbda2c06cd4d7e25706bc5dd1
Ahmet Bahar <ahmetbhr29@gmail.com>
2023-10-11 19:25:55 +0300

    Player Moving

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -2,17 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+	public class PlayerController : MonoBehaviour
+	{
+        public int PlayerHeal;
+        [SerializeField] private float movementSpeed;
         
-    }
+        [SerializeField] private float groundCheckDistance, wallCheckDistance;
+        [SerializeField] private Transform wallCheck, groundCheck;
+        [SerializeField] private LayerMask whatIsGround, whatIsCheck;
+        
+        private int facingDirection, damageDirection;
+        private Vector2 movement;
+        private bool groundDetected, wallDetected;
+        
+        private GameObject alive;
+        private Rigidbody2D aliveRb;
+        //private Animator aliveAnim;
+        
+        private void Start()
+        {
+            alive = transform.Find("PlayerBoudy").gameObject;
+            aliveRb = alive.GetComponent<Rigidbody2D>();
+         //   aliveAnim = alive.GetComponent<Animator>();
+            PlayerHeal = 1;
+            
+            facingDirection = 1;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void Update()
+        {
+            UpdateMovingState();
+            
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Damage();
+            }
+
+        }
         
-    }
+        private void UpdateMovingState()
+        {
+            wallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+
+            if(wallDetected)
+            {
+                Flip();
+            }
+            else
+            {
+                movement.Set(movementSpeed * facingDirection, aliveRb.velocity.y);
+                aliveRb.velocity = movement;
+            }
+        }
+
+        private void EnterDead()
+        {
+            Destroy(gameObject);
+        }
+        
+        private void Damage()
+        {
+            PlayerHeal--;
+
+            if(PlayerHeal <= 0)
+            {
+                EnterDead();
+            }
+        }
+        
+        private void Flip()
+        {
+            facingDirection *= -1;
+            alive.transform.Rotate(0.0f, 180.0f, 0.0f);
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+            Gizmos.DrawLine(wallCheck.position, new Vector2(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+        }
+	}
 }
+
+ 
+	
+
